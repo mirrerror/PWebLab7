83281c594d04aab74693c790ac6602bd3961f8f4
mirrerror <cayote20@mail.ru>
2025-05-14 10:56:09 +0300

    initial commit

---
+++ src/main/java/md/mirrerror/pweblab7/PWebLab7Application.java
@@ -0,0 +1,13 @@
+package md.mirrerror.pweblab7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PWebLab7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PWebLab7Application.class, args);
+    }
+
+}
---
+++ src/test/java/md/mirrerror/pweblab7/PWebLab7ApplicationTests.java
@@ -0,0 +1,13 @@
+package md.mirrerror.pweblab7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PWebLab7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
