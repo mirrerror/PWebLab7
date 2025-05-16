89a0550b8ba4520a5102011cba704b0bc3854d2d
mirrerror <cayote20@mail.ru>
2025-05-16 20:08:52 +0300

    add pagination and sorting

--- src/main/java/md/mirrerror/pweblab7/repositories/TVSeriesRepository.java
+++ src/main/java/md/mirrerror/pweblab7/repositories/TVSeriesRepository.java
@@ -1,13 +1,19 @@
 package md.mirrerror.pweblab7.repositories;
 
 import md.mirrerror.pweblab7.models.TVSeries;
+import md.mirrerror.pweblab7.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TVSeriesRepository extends CrudRepository<TVSeries, Long> {
     Optional<TVSeries> findByTitle(String name);
-
+    Page<TVSeries> findAll(Pageable pageable);
+    List<TVSeries> findAllByUser(User user);
+    Page<TVSeries> findAllByUser(User user, Pageable pageable);
 }
--- src/main/java/md/mirrerror/pweblab7/services/TVSeriesService.java
+++ src/main/java/md/mirrerror/pweblab7/services/TVSeriesService.java
@@ -2,7 +2,10 @@ package md.mirrerror.pweblab7.services;
 
 import lombok.RequiredArgsConstructor;
 import md.mirrerror.pweblab7.models.TVSeries;
+import md.mirrerror.pweblab7.models.User;
 import md.mirrerror.pweblab7.repositories.TVSeriesRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -19,6 +22,18 @@ public class TVSeriesService {
         List<TVSeries> series = new ArrayList<>();
         tvSeriesRepository.findAll().forEach(series::add);
         return series;
+    }
+
+    public Page<TVSeries> getAllSeries(Pageable pageable) {
+        return tvSeriesRepository.findAll(pageable);
+    }
+
+    public List<TVSeries> getSeriesByUser(User user) {
+        return tvSeriesRepository.findAllByUser(user);
+    }
+
+    public Page<TVSeries> getSeriesByUser(User user, Pageable pageable) {
+        return tvSeriesRepository.findAllByUser(user, pageable);
     }
 
     public Optional<TVSeries> getSeriesById(Long id) {
