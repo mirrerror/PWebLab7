3756da037f91a6a64d222b2078703ad5962e48ba
mirrerror <cayote20@mail.ru>
2025-05-17 17:52:51 +0300

    add swagger docs

--- src/main/java/md/mirrerror/pweblab7/controllers/UserController.java
+++ src/main/java/md/mirrerror/pweblab7/controllers/UserController.java
@@ -1,5 +1,7 @@
 package md.mirrerror.pweblab7.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import md.mirrerror.pweblab7.dtos.UserDto;
 import md.mirrerror.pweblab7.dtos.mappers.UserMapper;
@@ -16,6 +18,7 @@ import java.util.Optional;
 
 @RequestMapping("/api/users")
 @RestController
+@Tag(name = "Users", description = "Endpoints for managing users")
 @RequiredArgsConstructor
 public class UserController {
 
@@ -23,6 +26,7 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping("/me")
+    @Operation(summary = "Get authenticated user's info")
     public ResponseEntity<UserDto> authenticatedUser() {
         Optional<User> optionalUser = userService.getCurrentUser();
 
@@ -35,12 +39,14 @@ public class UserController {
 
     @GetMapping("/all")
     @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Get all users (Admin only)")
     public ResponseEntity<List<UserDto>> allUsers() {
         return ResponseEntity.ok(userMapper.mapToDtoList(userService.getAllUsers()));
     }
 
     @DeleteMapping("/delete/{id}")
     @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Delete user by ID (Admin only)")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         Optional<User> optionalUser = userService.getUserById(id);
 
